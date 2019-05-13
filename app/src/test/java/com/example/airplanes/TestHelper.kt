c26d747d2c6c07e522f06bb8f71607b24f965f73
Hotzi Péter <hotzipeti@gmail.com>
2019-05-13 13:31:48 +0200

    TestHelper modified

--- app/src/test/java/com/example/airplanes/TestHelper.kt
+++ app/src/test/java/com/example/airplanes/TestHelper.kt
@@ -6,7 +6,7 @@ import org.robolectric.shadows.ShadowLog
 val testInjector: TestComponent
     get() {
         ShadowLog.stream = System.out
-        val application = RuntimeEnvironment.application as KotifyDemoApplication
+        val application = RuntimeEnvironment.application as AirplanesApplication
         val component = DaggerTestComponent.builder().testModule(TestModule(application.applicationContext)).build()
         application.injector = component
         return component
